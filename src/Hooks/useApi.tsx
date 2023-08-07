658776974213973f7beff227a832e0a5e876e9b0
= <=>
2023-08-07 17:27:27 +0200

    save

--- src/Hooks/useApi.tsx
+++ src/Hooks/useApi.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import {
   AcceptCallRequest,
   AcceptCallResponse,
@@ -35,43 +36,71 @@ export const baseAddress =
   window.location.hostname === "localhost" ? "https://localhost:7072" : "https://monkfish-app-lzibp.ondigitalocean.app";
 const api = new Api({ baseUrl: baseAddress });
 
-const unpack = async <TOut,>(response: Promise<HttpResponse<TOut, any>>) => {
-  const r = await response;
-  //todo error handling
-  return r.data;
+const useErrorHandler = () => {
+  const navigate = useNavigate();
+  const onError = (code: number) => {
+    console.log("Error: " + code);
+    if (code === 401) {
+      navigate("/login");
+    } else {
+      navigate("/");
+    }
+  };
+  return { onError };
+};
+
+const unpack = async <TOut,>(
+  response: Promise<HttpResponse<TOut, any>>,
+  errorHandler?: {
+    onError: (code: number) => void;
+  }
+) => {
+  try {
+    const r = await response;
+    console.log({ r });
+    if (r.ok === false && errorHandler !== undefined) {
+      errorHandler.onError(r.status);
+    }
+    return r.data;
+  } catch (error) {
+    errorHandler?.onError(500);
+    return {};
+  }
 };
 
 export const useRoomsApi = () => {
+  const errorHandler = useErrorHandler();
   const proposeCall = (request: ProposeCallRequest): Promise<ProposeCallResponse> => {
-    return unpack(api.public.apiV10RoomsProposeCallCreate(request));
+    return unpack(api.public.apiV10RoomsProposeCallCreate(request), errorHandler);
   };
 
   const acceptCall = (request: AcceptCallRequest): Promise<AcceptCallResponse> => {
-    return unpack(api.public.apiV10RoomsAcceptCallCreate(request));
+    return unpack(api.public.apiV10RoomsAcceptCallCreate(request), errorHandler);
   };
 
   const getRoom = (request: GetRoomRequest): Promise<GetRoomResponse> => {
-    return unpack(api.public.apiV10RoomsGetRoomCreate(request));
+    return unpack(api.public.apiV10RoomsGetRoomCreate(request), errorHandler);
   };
 
   return { proposeCall, acceptCall, getRoom };
 };
 
 export const useUsersApi = () => {
+  const errorHandler = useErrorHandler();
   const register = (request: RegisterRequest): Promise<RegisterResponse> => {
-    return unpack(api.public.apiV10UsersRegisterCreate(request));
+    return unpack(api.public.apiV10UsersRegisterCreate(request), errorHandler);
   };
 
   const login = (request: LoginRequest): Promise<LoginResponse> => {
-    return unpack(api.public.apiV10UsersLoginCreate(request));
+    return unpack(api.public.apiV10UsersLoginCreate(request), errorHandler);
   };
 
   const getUsersList = (request: GetUsersListRequest): Promise<GetUsersListResponse> => {
-    return unpack(api.public.apiV10UsersGetUsersListCreate(request));
+    return unpack(api.public.apiV10UsersGetUsersListCreate(request), errorHandler);
   };
 
   const getUser = (request: GetUserRequest): Promise<GetUserResponse> => {
-    return unpack(api.public.apiV10UsersGetUserCreate(request));
+    return unpack(api.public.apiV10UsersGetUserCreate(request), errorHandler);
   };
 
   const getCurrentUser = (request: GetCurrentUserRequest): Promise<GetCurrentUserResponse> => {
@@ -82,32 +111,33 @@ export const useUsersApi = () => {
 };
 
 export const useWebRtcApi = () => {
+  const errorHandler = useErrorHandler();
   const getIceServers = (request: GetIceServersRequest): Promise<GetIceServersResponse> => {
-    return unpack(api.public.apiV10WebRtcGetIceServersCreate(request));
+    return unpack(api.public.apiV10WebRtcGetIceServersCreate(request), errorHandler);
   };
 
   const createRoom = (request: CreateRoomRequest): Promise<CreateRoomResponse> => {
-    return unpack(api.public.apiV10WebRtcCreateRoomCreate(request));
+    return unpack(api.public.apiV10WebRtcCreateRoomCreate(request), errorHandler);
   };
 
   const addCandidate = (request: AddCandidateRequest): Promise<AddCandidateResponse> => {
-    return unpack(api.public.apiV10WebRtcAddCandidateCreate(request));
+    return unpack(api.public.apiV10WebRtcAddCandidateCreate(request), errorHandler);
   };
 
   const setAnswer = (request: SetAnswerRequest): Promise<SetAnswerResponse> => {
-    return unpack(api.public.apiV10WebRtcSetAnswerCreate(request));
+    return unpack(api.public.apiV10WebRtcSetAnswerCreate(request), errorHandler);
   };
 
   const notifyCaller = (
     request: NotifyCallerAboutRoomConfiguredRequest
   ): Promise<NotifyCallerAboutRoomConfiguredResponse> => {
-    return unpack(api.public.apiV10WebRtcNotifyCallerAboutRoomConfiguredCreate(request));
+    return unpack(api.public.apiV10WebRtcNotifyCallerAboutRoomConfiguredCreate(request), errorHandler);
   };
 
   const notifyReceiver = (
     request: NotifyReceiverAboutRoomConfiguredRequest
   ): Promise<NotifyReceiverAboutRoomConfiguredResponse> => {
-    return unpack(api.public.apiV10WebRtcNotifyReceiverAboutRoomConfiguredCreate(request));
+    return unpack(api.public.apiV10WebRtcNotifyReceiverAboutRoomConfiguredCreate(request), errorHandler);
   };
 
   return { getIceServers, createRoom, addCandidate, setAnswer, notifyCaller, notifyReceiver };
